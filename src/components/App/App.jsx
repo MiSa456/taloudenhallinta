7e160e5fc5dc6bcecbfc749f999c0e563678c173
msalonen <p33155@edu.sasky.fi>
2025-01-02 21:50:08 +0200

    lisää buttons-kokoelman Button-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import Item from '../Item'
 import Header from '../Header'
 import Menu from '../Menu'
 import Content from '../Content'
+import Button from '../../shared/buttons'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
           <Item />
           <Item />
           <Item />
+          <Button> LISÄÄ UUSI RIVI</Button>
         </Content>
         <Menu />
       </div>
