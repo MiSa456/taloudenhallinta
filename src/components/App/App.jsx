4da2ebbc8c492e9a205b960b6215e8cbc1fd13f6
msalonen <p33155@edu.sasky.fi>
2025-01-03 22:20:53 +0200

    lisää Items-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
 import styles from './App.module.scss'
-import Item from '../Item'
 import Header from '../Header'
 import Menu from '../Menu'
 import Content from '../Content'
 import { FloatingButton } from '../../shared/buttons'
 import { ButtonContainer } from '../../shared/buttons'
+import Items from '../Items'
 
 function App() {
 
@@ -14,10 +14,7 @@ function App() {
       <div className={styles.app}>
         <Header />
         <Content>
-          <Item />
-          <Item />
-          <Item />
-          <Item />
+          <Items />
           <FloatingButton secondary>+</FloatingButton>
         </Content>
         <Menu />
