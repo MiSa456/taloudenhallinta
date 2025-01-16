c0c324db665f2f903e0eec7ecaeefd1454be1863
msalonen <p33155@edu.sasky.fi>
2025-01-16 20:56:01 +0200

    lisää AddItem- ja ItemForm-komponentit sekä reitityksen

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -4,6 +4,7 @@ import Items from '../Items'
 import Root from '../Root'
 import Settings from '../Settings'
 import Stats from '../Stats'
+import AddItem from '../AddItem'
 
 function AppRouter(props) {
   const router = createBrowserRouter([
@@ -16,7 +17,8 @@ function AppRouter(props) {
           element: <Items />, 
           loader: () => { return props.data } },
         { path: "stats", element: <Stats /> },
-        { path: "settings", element: <Settings /> }
+        { path: "settings", element: <Settings /> },
+        { path: "add", element: <AddItem /> }
       ]
     }
   ])
--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,7 +1,7 @@
 import styles from './Items.module.scss'
 import { FloatingButton } from '../../shared/buttons'
 import Item from '../Item/Item'
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 
 function Items() {
 
@@ -11,7 +11,7 @@ function Items() {
   return (
     <div className={styles.items}>
       { items }
-      <FloatingButton secondary>+</FloatingButton>
+      <Link to="/add"><FloatingButton secondary>+</FloatingButton></Link>
     </div>
   )
 
