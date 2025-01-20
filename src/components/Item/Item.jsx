c0f1b410aa1dfbab787a7a6368ba6d5ae0040ae2
msalonen <p33155@edu.sasky.fi>
2025-01-20 18:30:57 +0200

    lisää linkityksen merkinnän muokkaussivulle

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,5 +1,6 @@
 import styles from './Item.module.scss'
 import { MdNavigateNext } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 function Item({data, ...props}) {
 
@@ -32,6 +33,9 @@ function Item({data, ...props}) {
         <div className={styles.item_timespan}>{period}</div>
         <div className={styles.item_receiver}>{data.receiver}</div>
         <div className={styles.item_average}>{average ? average + "/kk" : ""}</div>
+        <div className={styles.item_edit}>
+        <Link to={"/edit/" + data.id}><MdNavigateNext /></Link>
+        </div>
       </div>
 
       <div className={styles.item_edit}>
